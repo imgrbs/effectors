07f45c67e95a8120872d2a04b4b7ac026bbc58dc
Imagine Rabbits <keerati.jear@gmail.com>
2020-04-18 22:37:48 +0700

    [ADD] add logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import { Router, Link } from 'react-static'
+import { Router, Link, Head } from 'react-static'
 import { hot } from 'react-hot-loader'
 //
 import Routes from 'react-static-routes'
+
 
 import './app.css'
 
 const App = () => (
   <Router>
     <div>
+      <Head>
+        <meta charSet="UTF-8" />
+        <title>Effectors - IMGRBS</title>
+        <link rel="icon" href='logo.png' />
+      </Head>
       <nav>
         <Link exact to="/">Home</Link>
       </nav>
